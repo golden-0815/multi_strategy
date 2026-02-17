dc7cfc5bb5818f5a2a464206de6c26e3bc97530c
golden-0815 <andreas.larsen062410@gmail.com>
2026-02-17 23:48:39 +0800

    chore: USDC sol file completed

---
+++ src/mocks/MockUSDC.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.27;
+
+import {ERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MockUSDC is ERC20 {
+    constructor() ERC20("Mock USDC", "USDC") {}
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amt) external {
+        _mint(to, amt);
+    }
+}
